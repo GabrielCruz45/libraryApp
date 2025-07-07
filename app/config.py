2bcaae18c41cd59af2f3d6f6dc6db0983835ee43
Gabriel Cruz <gabriel.crzprz45@gmail.com>
2025-07-07 06:49:21 -0400

    further app configuration

--- app/config.py
+++ app/config.py
@@ -2,3 +2,15 @@
 # This includes your SECRET_KEY, the database location (SQLALCHEMY_DATABASE_URI), and 
 # any other settings. This keeps sensitive information and settings separate from 
 # your application logic.
+
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'dev')
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite///' + os.path.join(basedir, 'libraryApp.db')
+    SQLACLHEMY_TRACK_MODIFICATIONS = False
--- run.py
+++ run.py
@@ -1,2 +1,14 @@
 # This is the entry point of our application. Its only job is to import and run our Flask app. 
 # You'll execute python run.py to start the server.
+
+from app import create_app, db
+
+app = create_app()
+
+
+@app.shell_context_processor
+def make_shell_processor():
+    return {'db': db}
+
+if __name__ == '__main__':
+    app.run(debug=True)
